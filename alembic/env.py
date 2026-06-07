081e10da512a93010c17a45a934393b6607e06dd
Ivan Maschenko <ivandenisovich2005@icloud.com>
2026-06-07 12:31:07 +0300

    chore: add database url import

--- alembic/env.py
+++ alembic/env.py
@@ -8,10 +8,12 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.db.session import Base
 from app.models import user  # noqa: F401 - ensures models are registered
+from app.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
